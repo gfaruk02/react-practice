4cbf76f3e9452c48712ce34ed82a25d3a98e366b
gfaruk02 <gfaruk02@gmail.com>
2026-09-05 22:10:43 +0600

    add react practice

---
+++ react-practice/src/TestComponent.jsx
@@ -0,0 +1,10 @@
+
+const name = "Golam Faruk"
+export default function TestComponent(){
+    return (
+        <>
+        <h1>Test Checked</h1>
+        <p> My name is {name} </p>
+        </>
+    )
+}
